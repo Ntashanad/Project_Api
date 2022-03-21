76f5aedb4c033d5ed51a9067896ffd4943913d02
Ntashanad <96516285+Ntashanad@users.noreply.github.com>
2022-03-21 16:12:26 +0800

    Update hello.php

--- web_api/src/Controllers/hello.php
+++ web_api/src/Controllers/hello.php
@@ -1,7 +1,7 @@
 <?php 
 use Slim\Http\Request; //namespace 
 use Slim\Http\Response; //namespace 
-// read table products 
+// to try the server
 $app->get('/hello/{name}', function (Request $request, Response $response,  array $arg){ 
  $name = $arg['name']; 
  $response->getBody() -> write ("Hello World, $name"); 
